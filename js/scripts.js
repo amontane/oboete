c6c268a4aaffc89bd16e9bdab64a3218cd78296f
Albert Montane <mummyfighter@gmail.com>
2024-05-28 12:59:33 +0200

    Add flip card and previous/next

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 var selectedKanjiUnits = "";
 var selectedLanguage = "cat";
 var wordData;
+var wordPointer = 0;
 
 function loadWords() {
 	var xhttp = new XMLHttpRequest();
@@ -8,7 +9,7 @@ function loadWords() {
 	    if (this.readyState == 4 && this.status == 200) {
 	       wordData = JSON.parse(xhttp.response);
 	       hideLoader();
-	       loadCards();
+	       loadCard(document);
 	    }
 	};
 	xhttp.open("GET", wordUrl(), true);
@@ -39,8 +40,57 @@ function hideLoader() {
 
 }
 
-function loadCards() {
+function loadCard(object) {
+	object.querySelectorAll('div[class="kanji"]')[0].innerHTML = wordData.words[wordPointer].word;
+	object.querySelectorAll('div[class="reading"]')[0].innerHTML = wordData.words[wordPointer].reading;
+	object.querySelectorAll('div[class="meaning"]')[0].innerHTML = wordData.words[wordPointer].meaning;
+}
 
+function flipCard(object) {
+	if (object.classList.contains("flipped")) {
+		object.classList.remove("flipped");
+	} else {
+		object.classList.add("flipped");
+	}
+}
+
+function nextCard() {
+	fakeCard("dismissed");
+	wordPointer = wordPointer + 1;
+	loadCard(document.querySelectorAll('div[class="flip-card"]')[0]);
+}
+
+function previousCard() {
+	var fake = fakeCard("presented");
+	wordPointer = wordPointer - 1;
+	loadCard(fake);
+}
+
+function fakeCard(className) {
+	var mainContainer = document.getElementById("main-body");
+	var oldCard = mainContainer.firstElementChild;
+
+	var callback = (className == "presented") ? destroyFakeAndLoad : destroyFake; 
+
+	var fakeCard = document.createElement("div");
+	fakeCard.innerHTML = oldCard.innerHTML;
+	fakeCard.className = oldCard.className + " fake " + className;
+	fakeCard.addEventListener("oanimationend", callback, false);
+	fakeCard.addEventListener("webkitAnimationEnd", callback, false);
+	fakeCard.addEventListener("animationend", callback, false);
+
+	mainContainer.appendChild(fakeCard);
+
+	return fakeCard
+}
+
+function destroyFake(element) {
+	element.currentTarget.remove();
+}
+
+function destroyFakeAndLoad(element) {
+	loadCard(document.querySelectorAll('div[class="flip-card"]')[0]);
+	element.currentTarget.remove();
 }
 
 function documentReady() {
